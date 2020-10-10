2ecce8c3227ff9d44d26b44fcd0afdb13d5e180c
sggh21 <64749203+sggh21@users.noreply.github.com>
2020-10-11 03:50:40 +0800

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -4,17 +4,17 @@
  This is the link to your domain...it redirects the user to your myaccount page.
  It is used on the site on buttons like: Buy now, Order now, Members, Buy HTTP proxies, etc.
 */
-export const MEMBERS_URL = "https://members.url.goes.here";
+export const MEMBERS_URL = "https://myaccount.prefects5.com";
 
 /*
  Your site name - this appears in the footer
 */
-export const SITE_NAME = "Site name goes here";
+export const SITE_NAME = "Prefects5";
 
 /*
  The name of you site that appears in Terms of service section - it can be you domain, your company name, etc.
 */
-export const TOS_SITE_NAME = 'whatever-you-need';
+export const TOS_SITE_NAME = 'Prefects5';
 
 /*
  The date when the terms of service where last updated.
@@ -24,7 +24,7 @@ export const TOS_LAST_UPDATE = 'Apr-27-2018';
 /*
  This is the link of your chat widget - it can be found on your tawk to account
 */
-export const TAWK_TO_WIDGET_URL = 'https://embed.tawk.to/5b61937de21878736ba285c5/default';
+export const TAWK_TO_WIDGET_URL = 'https://embed.tawk.to/5f82100ef0e7167d0017e820/default';
 
 /*
  This is the link to your backend services. It is used to get the data for prices and datacenters. Normally, you shouldn't touch this one
