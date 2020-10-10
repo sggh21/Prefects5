9d402a190413a91f968fe9ff20c442a54a017787
sggh21 <64749203+sggh21@users.noreply.github.com>
2020-10-11 03:42:19 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@
 /*
  The name of this site that we are current on.
 */
-const THIS_SITE_DOMAIN = 'http://your.site.domain';
+const THIS_SITE_DOMAIN = 'https://prefects5.com';
 
 module.exports = {
   siteMetadata: {
